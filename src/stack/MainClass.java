757c1904bc36824673dab68c801064130fa54a41
sasi <sasi@idea-PC>
2017-02-10 23:35:18 +0530

    Stack code

--- src/stack/MainClass.java
+++ src/stack/MainClass.java
@@ -2,11 +2,16 @@ package stack;
 
 public class MainClass {
 	public static void main(String[] args) throws Exception {
-		Stack s = new Stack(6);
+		LinkedListStack<Integer> s = new LinkedListStack<>();
 		s.push(2);
 		s.push(3);
 		s.push(20);
+		s.push(43);
+		s.push(22);
 		s.pop();
 		System.out.println(s);
+		System.out.println(s.peek());
+		s.pop();
+		System.out.println(s.peek());
 	}
 }
